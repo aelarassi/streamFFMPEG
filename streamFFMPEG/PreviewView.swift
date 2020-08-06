3d7092b5871a4a9a803bf139c1a449afe79e9585
a.elarassi <a.elarassi@dialy.net>
2020-08-06 15:01:38 +0100

    add camera UI fucs

---
+++ streamFFMPEG/PreviewView.swift
@@ -0,0 +1,33 @@
+//
+//  PreviewView.swift
+//  FFmpegIOS
+//
+//  Created by ABDELAZiZ EL ARASSI on 8/3/20.
+//  Copyright © 2020 ABDELAZiZ EL ARASSi. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PreviewView: UIView {
+
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        guard let layer = layer as? AVCaptureVideoPreviewLayer else {
+            fatalError("Expected `AVCaptureVideoPreviewLayer` type for layer. Check PreviewView.layerClass implementation.")
+        }
+        return layer
+    }
+
+    var session: AVCaptureSession? {
+        get {
+            return videoPreviewLayer.session
+        }
+        set {
+            videoPreviewLayer.session = newValue
+        }
+    }
+
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+}
