d55db742cb9b27c0a37c88e5e7b68bb8545a522d
a.elarassi <a.elarassi@dialy.net>
2020-08-06 14:52:07 +0100

    Initial Commit

---
+++ streamFFMPEG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  streamFFMPEG
+//
+//  Created by ABDELAZiZ EL ARASSI on 8/6/20.
+//  Copyright © 2020 ABDELAZiZ EL ARASSi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
